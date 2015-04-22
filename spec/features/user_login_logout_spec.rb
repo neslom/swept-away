83f3a150e9cdaab5e5e6268e4b4b200cd71065fa
Markus Olsen <molsen13@gmail.com>
2015-04-22 13:31:28 -0600

    moved oauth login to spec helper

--- spec/features/user_login_logout_spec.rb
+++ spec/features/user_login_logout_spec.rb
@@ -1,26 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "user logs in via omniauth" do
-
-  def login_with_oauth(service = :twitter)
-    visit "/auth/#{service}"
-  end
-
-  before(:each) do
-    Capybara.default_host = "http://localhost:3000"
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-      uid: "123456789",
-      info: {
-      name: "Markus",
-      image: "http://pbs.twimg.com/profile_images/542764285747662848/BRmaGBPU_normal.jpeg",
-      },
-      credentials: {
-      token: "2442504061-52NtnkpgGzSDtwKr74l7tZ10XVNt3VfVqDbeXg6"
-      }
-    })
-  end
-
   scenario "creates new user" do
     expect(User.count).to eq(0)
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+Dir[File.dirname(__FILE__) + "/helpers/**/*.rb"].each {|f| require f }
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
