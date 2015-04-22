b74d54581a3411b5bddfc2bd2b1562a1db7ad809
Markus Olsen <molsen13@gmail.com>
2015-04-22 13:20:10 -0600

    Testing omniauth login

--- spec/features/omni_auth_login_spec.rb
+++ spec/features/omni_auth_login_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
 RSpec.feature "user logs in via omniauth" do
+
   def login_with_oauth(service = :twitter)
     visit "/auth/#{service}"
   end
 
+  before(:each) do
+    Capybara.default_host = "http://localhost:3000"
+
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      uid: "123456789",
+      info: {
+      name: "Markus",
+      image: "http://pbs.twimg.com/profile_images/542764285747662848/BRmaGBPU_normal.jpeg",
+      },
+      credentials: {
+      token: "2442504061-52NtnkpgGzSDtwKr74l7tZ10XVNt3VfVqDbeXg6"
+      }
+    })
+  end
+
   scenario "creates new user" do
-    visit root_path
+    expect(User.count).to eq(0)
 
-    click_link_or_button("Sign in with Twitter")
+    login_with_oauth
 
-    expect(page).to have_content("Welcome!")
+    expect(page).to have_link("Logout")
+    expect(User.count).to eq(1)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,3 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 end
-
-Capybara.default_host = "http://localhost:3000"
-
-OmniAuth.config.test_mode = true
-OmniAuth.config.add_mock(:twitter, {
-  uid: "123456789",
-  name: "Markus",
-  image_url: "http://pbs.twimg.com/profile_images/542764285747662848/BRmaGBPU_normal.jpeg",
-  token: "2442504061-52NtnkpgGzSDtwKr74l7tZ10XVNt3VfVqDbeXg6"
-})
-
