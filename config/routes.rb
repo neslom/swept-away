bc5d1cf7790692cf753f4a472e698ff413b261ee
Markus Olsen <molsen13@gmail.com>
2015-04-21 19:44:46 -0600

    Add logout path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: 'sessions#create'
   root 'dashboard#index'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 end
