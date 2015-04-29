767bf6f7882eaad3f2a07dacf5458542bea9e7ed
Markus Olsen <molsen13@gmail.com>
2015-04-29 17:14:16 -0600

    Customized map popup

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("a").click(function(e) {
     var target = $(this).attr('href');
     e.preventDefault();
-    $("html, body").animate({
+    $("body").animate({
       scrollTop: $(target).offset().top
     }, 1500);
   });
