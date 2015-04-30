f037818188de6a7a61654b68f071f7adda5ff087
Markus Olsen <molsen13@gmail.com>
2015-04-29 20:33:52 -0600

    Added test for clicking around on map... in my dreams

--- spec/features/initial_user_input_spec.rb
+++ spec/features/initial_user_input_spec.rb
@@ -13,4 +13,14 @@ RSpec.feature "user enters ward and section to see possible dates" do
       expect(page).to have_content(x)
     end
   end
+
+  xscenario "user clicks ward on map and is shown schedules", js: true do
+    login_with_oauth
+
+    click_link_or_button("#{sweep.ward_id}"[1])
+
+    ["Your area's street sweeping schedule", sweep.print_month, sweep.print_days].each do |x|
+      expect(page).to have_content(x)
+    end
+  end
 end
