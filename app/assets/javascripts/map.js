a8f2562e88d8d4fb2d0066a4f6262dac8ffd6285
Markus Olsen <molsen13@gmail.com>
2015-04-29 20:21:29 -0600

    Managed to add custom popup links

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   L.mapbox.accessToken = 'pk.eyJ1IjoibmVzbG9tIiwiYSI6ImJDd2pwVHMifQ.3BbhgKs-v8-O0Uy2qF179g';
   var map = L.mapbox.map('map', 'neslom.m1o8j7i1')
-  .setView([41.854, -87.623], 10);
+    .setView([41.854, -87.623], 10);
 
   map.touchZoom.disable();
   map.scrollWheelZoom.disable();
 
-  $(".leaflet-clickable").click(function() {
-    alert("hello");
+  map.featureLayer.on("layeradd", function(e) {
+    var marker = e.layer;
+    var ward = marker.feature.properties.WARD;
+
+    var popupContent = "<a href='#' class='ward-submit'>" + ward + "</a>";
+
+    marker.bindPopup(popupContent);
   });
 });
