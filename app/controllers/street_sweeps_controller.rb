08b59de15edc6d5d226788b2fa66dfc1854fb86d
Markus Olsen <molsen13@gmail.com>
2015-05-06 15:45:14 -0600

    User sections action for assigning schedules

--- app/controllers/street_sweeps_controller.rb
+++ app/controllers/street_sweeps_controller.rb
@@ -1,8 +1,17 @@
 class StreetSweepsController < ApplicationController
   respond_to :json, :html
+  before_action :logged_in?, only: [:user_sections]
 
-  def show
+  def schedule
     @schedules = StreetSweep.schedules(params[:id]).to_json
     respond_with @schedules
+  end
+
+  def user_sections
+    ss = StreetSweep.find_by(ward_section: params[:wardSection])
+    if ss
+      current_user.street_sweeps << ss
+    end
+    render nothing: true, status: :ok
   end
 end
