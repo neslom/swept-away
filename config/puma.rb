26b79c18109b39db65ad52fbda600ed3c80070be
Markus Olsen <molsen13@gmail.com>
2015-05-07 08:47:32 -0600

    It appears that the worker boot code in the puma config was not necessary and that the redis global variable is being set properly in the redis initializer file

--- config/puma.rb
+++ config/puma.rb
@@ -9,10 +9,8 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   #if defined?(Resque)
-    #Resque.redis = ENV["REDISCLOUD_URL"]# || "redis://127.0.0.1:6379"
+    #Resque.redis = ENV["REDISCLOUD_URL"] || "redis://127.0.0.1:6379"
   #end
   ActiveRecord::Base.establish_connection
 end
